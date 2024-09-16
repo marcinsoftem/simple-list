f973a1e61423000e5708dbbbbbee6df4d6587b9a
Marcin Mikłas <m.miklas@wszib.edu.pl>
2024-09-16 21:46:54 +0200

    Dodano plik modułu web oraz backend

---
+++ backend/src/main/java/pl/softem/simplelist/SimpleListApplication.java
@@ -0,0 +1,13 @@
+package pl.softem.simplelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleListApplication.class, args);
+    }
+
+}
---
+++ web/src/components/List.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref, useTemplateRef, onMounted, onUpdated } from "vue";
+let element = ref("");
+let list = ref(null);
+const elementInput = useTemplateRef("elementInput");
+const ELEMENTS_URL = import.meta.env.VITE_API_URL + "/elements";
+
+onMounted(() => {
+  getElements();
+  setElementInputFocus();
+});
+
+onUpdated(() => {
+  setElementInputFocus();
+});
+
+function setElementInputFocus() {
+  elementInput.value.focus();
+}
+
+function getElements() {
+  fetch(ELEMENTS_URL, {
+    headers: {
+      // "Access-Control-Allow-Origin": "*",
+    }
+  })
+      .then((response) => response.json())
+      .then((data) => {
+        list.value = data
+        console.log("Getting elements: " + list.value);
+      })
+      .catch(function (error) {
+        console.log("Getting elements error: " + error);
+      });
+}
+
+function addElement() {
+  if (element.value == "") return;
+  fetch(ELEMENTS_URL, {
+    method: "POST",
+    headers: {
+      // "Access-Control-Allow-Origin": "*",
+    },
+    body: element.value,
+  }).then(function (response) {
+    if (response.ok) {
+      console.log("Adding element: " + element.value);
+      element.value = "";
+      getElements();
+    }
+  }).catch(function (error) {
+    console.log("Adding element error: " + error);
+  });
+}
+
+function removeElement(index) {
+  fetch(ELEMENTS_URL +"/" + index,  {
+    method: "DELETE",
+    headers: {
+      // "Access-Control-Allow-Origin": "*",
+    }
+  }).then(function (response) {
+    if (response.ok) {
+      console.log("Deleting element: " + list.value[index]);
+      getElements();
+    }
+  }).catch(function (error) {
+    console.log("Deleting element error: " + error);
+  });
+}
+</script>
+
+<template>
+  <div class="column is-one-third">
+    <div class="box">
+      <h1 class="title">Elementy listy</h1>
+      <p class="notification is-primary" v-for="(element, index) in list"> {{ element }}
+        <button class="delete" v-on:click="removeElement(index)"></button>
+      </p>
+      <div class="field is-grouped">
+        <input type="text" class="input" v-model="element" v-on:keyup.enter="addElement" placeholder="Wprowadź element" ref="elementInput"/>
+        <button class="button is-link" v-on:click="addElement">Dodaj</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
